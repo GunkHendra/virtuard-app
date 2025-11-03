64044ae98e6b8e45c6461dcaa52bf923e92c879e
Gung Hendra <gunghencrypt@gmail.com>
2025-11-03 23:17:23 +0800

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,116 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { icons } from "@/constants/icons";
+import { Tabs } from "expo-router";
+import { Image, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+function TabIcon({ icon }: any) {
+    return (
+        <View>
+            <Image source={icon} tintColor="#151312" className="size-6" />
+        </View>
+    );
+}
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+function HeaderIcon() {
+    const insets = useSafeAreaInsets();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <View
+            style={{ paddingTop: 20 + insets.top, paddingHorizontal: 16 }}
+            className="bg-white px-4 py-4 flex-row items-center justify-between"
+        >
+            <TouchableOpacity onPress={() => console.log("Menu pressed")}>
+                <Image
+                    source={icons.arrow}
+                    className="size-6"
+                    tintColor="#151312"
+                />
+            </TouchableOpacity>
+
+            <View className="flex-1 items-center">
+                <Image
+                    source={icons.logo}
+                    className="h-8 w-24"
+                    resizeMode="contain"
+                />
+            </View>
+
+            <TouchableOpacity onPress={() => console.log("Profile pressed")}>
+                <Image
+                    source={icons.person}
+                    className="size-10 rounded-full"
+                />
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+export default function Layout() {
+    const insets = useSafeAreaInsets();
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    height: 60 + insets.bottom,
+                    paddingTop: 15,
+                    paddingBottom: insets.bottom,
+                    justifyContent: "center",
+                    alignItems: "center",
+                },
+                headerShown: true,
+                header: () => <HeaderIcon />
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: () => (
+                        <TabIcon icon={icons.home} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="hotspot"
+                options={{
+                    title: "Hotspot",
+                    tabBarIcon: () => (
+                        <TabIcon icon={icons.search} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="plus"
+                options={{
+                    title: "Plus",
+                    tabBarIcon: () => (
+                        <TabIcon icon={icons.person} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="saved"
+                options={{
+                    title: "Saved",
+                    tabBarIcon: () => (
+                        <TabIcon icon={icons.save} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    tabBarIcon: () => (
+                        <TabIcon icon={icons.person} />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
---
+++ app/(tabs)/hotspot.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const hotspot = () => {
+  return (
+    <View>
+      <Text>hotspot</Text>
+    </View>
+  )
+}
+
+export default hotspot
+
+const styles = StyleSheet.create({})
